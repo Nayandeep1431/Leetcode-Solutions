46c001c1c57f1f884b95584dae92de080f326b2d
Nayandeep1431 <154627577+Nayandeep1431@users.noreply.github.com>
2025-06-20 21:02:44 +0530

    Create Construct String from Binary Tree.cpp
    Construct String from Binary Tree
    Leetcode (606)
    
    Problem Link : https://leetcode.com/problems/construct-string-from-binary-tree/description/
    
    Difficulty Level : Medium

---
+++ Recursion and Backtracking/Construct String from Binary Tree.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string tree2str(TreeNode* root) {
+        if(root == NULL)
+            return "" ;
+
+        string ans = to_string(root -> val) ;
+        string l = tree2str(root -> left) ;
+        string r = tree2str(root -> right) ;
+
+        if(!root -> left && !root -> right)
+            return ans ;
+        if(!root -> right)
+           return ans + "("+ l +")" ;
+        if(!root -> left)
+            return ans + "(" + ")" + "("+r+")" ;
+
+        return ans + "("+ l+ ")" + "("+ r+")" ;               
+    }
+};
