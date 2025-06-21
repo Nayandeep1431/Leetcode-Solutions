c3f88831176f21cb17674ab3e7a7dabca7106589
Nayandeep1431 <154627577+Nayandeep1431@users.noreply.github.com>
2025-06-21 11:10:52 +0530

    Create Minimum Deletions to Make Character Frequencies Unique.cpp
    Minimum Deletions to Make Character Frequencies Unique
    Leetcode (1647)
    
    Problem Link : https://leetcode.com/problems/minimum-deletions-to-make-character-frequencies-unique/description/
    
    Difficulty Level : Medium

---
+++ Strings/Minimum Deletions to Make Character Frequencies Unique.cpp
@@ -0,0 +1,27 @@
+
+class Solution
+{
+public:
+    int minDeletions(string s)
+    {
+        vector<int> freq(26, 0);
+
+        for (char c : s)
+            freq[c - 'a']++;
+
+        sort(freq.begin(), freq.end(), greater<int>());
+
+        int deletions = 0;
+
+        for (int i = 1; i < 26; i++)
+        {
+            while (freq[i] != 0 && freq[i] >= freq[i - 1])
+            {
+                freq[i]--;
+                deletions++;
+            }
+        }
+
+        return deletions;
+    }
+};
