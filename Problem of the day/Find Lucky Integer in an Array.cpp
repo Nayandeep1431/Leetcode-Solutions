7443cf1667287b74afae5408c734fb44658f9bdd
Nayandeep1431 <154627577+Nayandeep1431@users.noreply.github.com>
2025-07-05 09:40:43 +0530

    Create Find Lucky Integer in an Array.cpp
    Find Lucky Integer in an Array
    Leetcode 1394
    
    Date : 5th July 2025
    
    Problem Link : https://leetcode.com/problems/find-lucky-integer-in-an-array/description/?envType=daily-question&envId=2025-07-05
    
    Difficulty Level : Easy

---
+++ Problem of the day/Find Lucky Integer in an Array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findLucky(vector<int>& arr) {
+        int n = arr.size() ;
+        map<int, int> mpp ;
+        for(int i =0 ; i< n ; i++){
+            mpp[arr[i]]++ ;
+        }
+        int maxi = -1 ;
+        for(auto it : mpp){
+            if(it.first == it.second)
+                maxi = it.first ;
+        }
+        return maxi ;
+    }
+};
