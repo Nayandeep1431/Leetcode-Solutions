ae8a77fed9e2a7067f868f28f858c0f07b149a20
Nayandeep1431 <154627577+Nayandeep1431@users.noreply.github.com>
2025-09-03 12:54:00 +0530

    Create Find the Number of Ways to Place People II.cpp
    Find the Number of Ways to Place People II
    Leetcode : 3027 
    
    Date : 3rd September 2025
    
    Problem Link : https://leetcode.com/problems/find-the-number-of-ways-to-place-people-ii/description
    
    Difficulty Level : Hard

---
+++ Problem of the day/Find the Number of Ways to Place People II.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    static bool cmp(const vector<int> &a ,const vector<int> &b){
+        if(a[0] == b[0])
+            return a[1] > b[1] ;
+        else
+            return a[0] < b[0] ;    
+    }
+    int numberOfPairs(vector<vector<int>>& points) {
+        int n = points.size();
+        int cnt = 0;
+
+        sort(points.begin() , points.end() , cmp) ;
+        for(int i =0 ; i < n ; i++){
+            int x1 = points[i][0] ;
+            int y1 = points[i][1] ;
+
+            int Y = INT_MIN ;
+            for(int j = i+1 ; j < n ; j++){
+                int x2 = points[j][0] ;
+                int y2 = points[j][1] ;
+
+                if(y2 > y1)
+                    continue ;
+
+                if(y2 > Y){
+                    cnt++ ;
+                    Y = y2 ;
+                }    
+            }
+        }
+        return cnt;
+    }
+};
