46022962c3c9f595f1436cd704bf643db71368aa
Nayandeep1431 <154627577+Nayandeep1431@users.noreply.github.com>
2026-07-14 00:24:01 +0530

    Create Online Stock Span.cpp
    Online Stock Span
    Leetcode : 901 
    Level : Medium

---
+++ Stack/Online Stock Span.cpp
@@ -0,0 +1,16 @@
+class StockSpanner {
+public:
+    StockSpanner() {
+        
+    }
+    stack<pair<int , int>> st ; 
+    int next(int price) {
+        int val = 1 ; 
+        while(!st.empty() && st.top().first <= price){
+            val += st.top().second ; 
+            st.pop() ;
+        }
+        st.push({price , val}) ; 
+        return val ; 
+    }
+};
