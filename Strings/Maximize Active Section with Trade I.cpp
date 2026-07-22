2b3589f8c1b1e6573d1d5a00e65084876ad2572b
Nayandeep1431 <154627577+Nayandeep1431@users.noreply.github.com>
2026-07-22 16:44:36 +0530

    Create Maximize Active Section with Trade I.cpp
    Maximize Active Section with Trade I
    Leetcode : 3499 
    Level : Medium

---
+++ Strings/Maximize Active Section with Trade I.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxActiveSectionsAfterTrade(string s) {
+       int n = s.length() ; 
+
+       int ones = count(s.begin() , s.end() , '1') ; 
+       vector<int> inactive ; 
+       int i = 0 ; 
+       while(i < n){
+        if(s[i] == '0'){
+            int start = i ; 
+            while(i < n && s[i] == '0'){
+                i++ ; 
+            }
+            inactive.push_back(i - start) ;
+        }
+        else
+            i++ ;
+       }
+       int maxi = 0 ; 
+       for(int i = 1 ; i< inactive.size() ; i++){
+        maxi = max(maxi , inactive[i] + inactive[i-1]) ; 
+       }
+       return maxi + ones ; 
+    }
+};
