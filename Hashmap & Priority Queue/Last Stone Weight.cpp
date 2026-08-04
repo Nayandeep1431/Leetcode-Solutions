342c51d179954be193d802b2cc1cf30191a38bca
Nayandeep1431 <154627577+Nayandeep1431@users.noreply.github.com>
2026-08-04 21:05:03 +0530

    Create Last Stone Weight.cpp
    Last Stone Weight
    Leetcode : 1046
    Level : Easy

---
+++ Hashmap & Priority Queue/Last Stone Weight.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        int n = stones.size() ; 
+        priority_queue<int> pq ; 
+        for(int i =0 ; i< n ; i++){
+            pq.push(stones[i]) ; 
+        }
+        while(pq.size() > 1){
+            int t1 = pq.top() ; pq.pop() ; 
+            int t2 = pq.top() ; pq.pop() ; 
+
+            if(t1 == t2)
+                continue ; 
+            
+            pq.push(abs(t1 -t2)) ; 
+        }
+        return (pq.size() == 0) ? 0 : pq.top() ; 
+    }
+};
